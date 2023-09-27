e28b7614ce913fcceaf60e2d192f113504c14fb6
VarizkyNaldiba <138732792+VarizkyNaldiba@users.noreply.github.com>
2023-09-27 07:59:08 +0700

    Push Modifikas Percobaan 1

--- Jobsheet5/PemilihanPercobaan128.java
+++ Jobsheet5/PemilihanPercobaan128.java
@@ -3,12 +3,13 @@ import java.util.Scanner;
 public class PemilihanPercobaan128 {
     public static void main(String[] args) {
         Scanner input29=new Scanner(System.in);
+
         System.out.println("Masukkan Angka: ");
         int angka = input29.nextInt();
-        if (angka % 2 == 0)
-            System.out.println("Angka " +angka+ " bilangan genap");
-        else 
-            System.out.println("Angka " +angka+ " bilangan ganjil");
+        
+        String hasil = (angka % 2 == 0) ? "Genap" : "Ganjil";
+        System.out.println("Angka " + angka + " bilangan " + hasil);
 
+    
     }
 }
