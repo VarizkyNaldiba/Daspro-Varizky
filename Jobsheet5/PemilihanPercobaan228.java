487c3ea861db3e0e01ee19d238d541f14aee5c4f
VarizkyNaldiba <138732792+VarizkyNaldiba@users.noreply.github.com>
2023-09-27 08:47:22 +0700

    Modifikasi Percobaan 2

--- Jobsheet5/PemilihanPercobaan228.java
+++ Jobsheet5/PemilihanPercobaan228.java
@@ -2,21 +2,35 @@ import java.util.Scanner;
 
 public class PemilihanPercobaan228 {
     public static void main(String[] args) {
-        Scanner input29=new Scanner(System.in);
+        Scanner input28=new Scanner(System.in);
 
         System.out.println("Nilai UAS       : ");
-        float UAS=input29.nextFloat();
+        float UAS=input28.nextFloat();
         System.out.println("Nilai UTS       : ");
-        float UTS=input29.nextFloat();
+        float UTS=input28.nextFloat();
         System.out.println("Nilai Kuis      : ");
-        float Kuis=input29.nextFloat();
+        float Kuis=input28.nextFloat();
         System.out.println("Nilai Tugas     : ");
-        float Tugas=input29.nextFloat();
+        float Tugas=input28.nextFloat();
 
         float Total = (UAS*0.4F) + (UTS*0.3F) + (Kuis*0.1F) + (Tugas*0.2F);
-        String Message=Total < 65 ? "Remidi" : " Tidak Remidi ";
+        System.out.println("Nilai Total anda adalah : " + Total);
 
-        System.out.println("Nilai Akhir = " + Total + " sehingga " + Message );
+        if (Total > 80) {
+            System.out.println("Nilai Anda adalah A");
+        } else if (Total <= 80) {
+            System.out.println("Nilai Anda Adalah B+");
+        } else if (Total <= 73) {
+            System.out.println("Nilai Anda Adalah B");
+        }else if (Total <= 65) {
+            System.out.println("Nilai Anda Adalah C+");
+        }else if (Total <= 60) {
+            System.out.println("Nilai Anda Adalah C");
+        }else if (Total <= 50) {
+            System.out.println("Nilai Anda Adalah D");
+        }else if (Total <= 39) {
+            System.out.println("Nilai Anda Adalah E");
+        }
 
     }
 }
