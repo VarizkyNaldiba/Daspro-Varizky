a3bcfdc08a189ff59fc377c8b9c1481fc721487d
VarizkyNaldiba <138732792+VarizkyNaldiba@users.noreply.github.com>
2023-10-02 19:21:39 +0700

    Pertanyaan Percobaan 1

---
+++ Jobsheet6/Pemilihan2Percobaan128.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan128 {
+    public static void main(String[] args) {
+        Scanner input28 = new Scanner(System.in);
+
+        int tahun;
+
+        System.out.println("Masukkan Tahun  :");
+        tahun=input28.nextInt();
+
+        if (( tahun % 4 ) == 0 ) {
+            if (( tahun % 100 ) != 0);
+                System.out.println("Tahun" + tahun + " Merupakan Tahun Kabisat");
+        } else 
+                System.out.println("Tahun" + tahun + "Bukan Tahun Kabisat");
+    }  
+}
