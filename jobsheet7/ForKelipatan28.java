890207946630935b9ecee0404b46708ce2ae6ba2
VarizkyNaldiba <138732792+VarizkyNaldiba@users.noreply.github.com>
2023-10-14 10:10:45 +0700

    Modifikasi percobaan 1 pertanyaan 3

--- jobsheet7/ForKelipatan28.java
+++ jobsheet7/ForKelipatan28.java
@@ -4,7 +4,7 @@ public class ForKelipatan28 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int kelipatan, jumlah=0, counter=0;
+        int kelipatan, jumlah=0, counter=0, mean=0;
 
         System.out.println("Masukkan bilangan kelipatan (1-9) : ");
         kelipatan = scan.nextInt();
@@ -13,9 +13,12 @@ public class ForKelipatan28 {
             if (i % kelipatan == 0 ) {
                 jumlah += i;
                 counter++;
+                mean = jumlah / counter;
+
             }
         }
         System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
         System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50  adalah %d\n", kelipatan, jumlah);
+        System.out.printf("Rata rata bilangan kelipatan %d dari 1 sampai 50 adalah %d\n" , kelipatan, mean);
     }
 }
