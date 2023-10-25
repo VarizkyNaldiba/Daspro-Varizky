54c9bd5de0950f16486a3175d7036d2fe7dd2d28
VarizkyNaldiba <138732792+VarizkyNaldiba@users.noreply.github.com>
2023-10-25 10:12:41 +0700

    Percobaan 4

--- Jobsheet9/ArrayRataNilai28.java
+++ Jobsheet9/ArrayRataNilai28.java
@@ -5,17 +5,12 @@ public class ArrayRataNilai28 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int jum = 0;
-        int[] nilaiMhs = new int[jum];
+        int[] nilaiMhs = new int[10];
         double total = 0;
         double rata2, rata2lulus = 0, rata2tdklulus = 0;
         int lulus = 0, tdklulus = 0;
 
-        
-
         for (int i = 0; i < nilaiMhs.length; i++) {
-            System.out.println("Masukkan jumlah Mahasiswa");
-            jum = sc.nextInt();
             System.out.println("Masukkan nilai Mahasiswa ke-" + (i+1) + " : ");
             nilaiMhs[i] = sc.nextInt();
 
