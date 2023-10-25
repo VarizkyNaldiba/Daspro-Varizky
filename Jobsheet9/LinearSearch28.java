6f06972dbbdd2c8071cabfbc31474723171ecccb
VarizkyNaldiba <138732792+VarizkyNaldiba@users.noreply.github.com>
2023-10-25 10:39:25 +0700

    Percobaan 4 Jobsheet9

--- Jobsheet9/LinearSearch28.java
+++ Jobsheet9/LinearSearch28.java
@@ -1,17 +1,29 @@
+import java.util.Scanner;
+
 public class LinearSearch28 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah elemen array: ");
+        int n = sc.nextInt();
+
+        int[] arrayInt = new int[n];
         
-        int [] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
-        int key = 20;
-        int hasil = 0;
+        for (int i = 0; i < n; i++) {
+            System.out.print("Masukkan elemen array ke-" + (i+1) + ": ");
+            arrayInt[i] = sc.nextInt();
+        }
 
-        for (int i = 0 ; i < arrayInt.length; i++) {
-            if (arrayInt[i] == key ) {
-                hasil = i;
+        System.out.print("Masukkan key yang ingin dicari: ");
+        int key = sc.nextInt();
+
+        int hasil = -1;
+        for (int i = 0; i < arrayInt.length; i++) {
+            if (arrayInt[i] == key) {
+                hasil = i + 1;
                 break;
-
             }
         }
-        System.out.println( " Key ada dalam  array pada posisi indeks ke- " + hasil);
+            System.out.println("Key ada di posisi indeks " + hasil);  
     }
 }
