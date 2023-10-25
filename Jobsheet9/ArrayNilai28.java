d66ceb8e18ffdd3ff38e91e7d6a0d0e932345521
VarizkyNaldiba <138732792+VarizkyNaldiba@users.noreply.github.com>
2023-10-25 08:00:19 +0700

    Percobaan 2 Jobsheet 9 Selesai

--- Jobsheet9/ArrayNilai28.java
+++ Jobsheet9/ArrayNilai28.java
@@ -1,18 +1,21 @@
-package Jobsheet9;
-import java.util.Scanner;
+    package Jobsheet9;
+    import java.util.Scanner;
 
-public class ArrayNilai28 {
-    public static void main(String[] args) {
+    public class ArrayNilai28 {
+        public static void main(String[] args) {
 
-        int[] nilaiAkhir = new int[10];
-        Scanner input = new Scanner(System.in);
-        
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Masukkan nilai akhir ke-" +i+ " : ");
-            nilaiAkhir[i] = input.nextInt(); 
-        }
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai Akhir ke-" +i+ " adalah " + nilaiAkhir[i]);
+            int[] nilaiAkhir = new int[10];
+            Scanner input = new Scanner(System.in);
+            
+            for (int i = 0; i < nilaiAkhir.length; i++) {
+                System.out.println("Masukkan nilai akhir ke-" +i+ " : ");
+                nilaiAkhir[i] = input.nextInt(); 
+            }
+            for (int i = 0; i < nilaiAkhir.length; i++) {
+                if (nilaiAkhir[i] > 70 ) {
+                System.out.println("Mahasiswa ke-" +i+ " Lulus! Dengan Nilai " + nilaiAkhir[i] );
+                } else
+                System.out.println("Mahasiswa ke-" +i+ " tidak Lulus! Dengan Nilai " + nilaiAkhir[i] );
+            }
         }
     }
-}
