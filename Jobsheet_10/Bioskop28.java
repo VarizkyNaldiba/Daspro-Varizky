55fbade8606434b673b5c85cf569737e1f553e9a
VarizkyNaldiba <138732792+VarizkyNaldiba@users.noreply.github.com>
2023-10-31 06:58:01 +0700

    Percobaaan 1 Pertanyaan 12 JS10

--- Jobsheet_10/Bioskop28.java
+++ Jobsheet_10/Bioskop28.java
@@ -19,10 +19,12 @@ public class Bioskop28 {
         System.out.printf("%s \t %s\n", penonton[2][0], penonton[2][1]);
         System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
 
-        System.out.println("Penonton pada baris ke-3: ");
-
-        for (String i  : penonton[2]) {
-            System.out.println(i);
+        System.out.println(penonton.length);
+        for (String[] barisPenonton : penonton) {
+            System.out.println("Panjang baris : " + barisPenonton.length);
+        }
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("penonton pada baris ke-" + (i+1) + " : " + String.join(", ", penonton[i]));
         }
     }
 }
