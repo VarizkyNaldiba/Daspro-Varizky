c6dc663e91e22e607ce1cd7e41580c674db1fdd8
VarizkyNaldiba <138732792+VarizkyNaldiba@users.noreply.github.com>
2023-11-08 06:41:29 +0700

    percobaan 4 JS11

---
+++ Jobsheet_11/Quiz.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Quiz {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+
+        char menu = 'y';
+
+        do {
+            int number = random.nextInt(10) + 1;
+            boolean success = false;
+            do {
+                System.out.println("Tebak Angka (1 - 10) : ");
+                int answer = sc.nextInt();
+                sc.nextLine();
+                success = (answer == number);
+            } while (!success);
+            System.out.println("Apakah anda ingin mengulang permainan (Y/y) ? ");
+            menu = sc.nextLine().charAt(0);
+        }while (menu == 'y' || menu == 'Y');
+    }
+}
