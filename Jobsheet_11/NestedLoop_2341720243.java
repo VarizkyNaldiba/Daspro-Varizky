6338604c7290f8745b9b6be1c2d2b83fbd7bf521
VarizkyNaldiba <138732792+VarizkyNaldiba@users.noreply.github.com>
2023-11-08 08:44:51 +0700

    percobaan 5 selesai

--- Jobsheet_11/NestedLoop_2341720243.java
+++ Jobsheet_11/NestedLoop_2341720243.java
@@ -17,11 +17,16 @@ public class NestedLoop_2341720243 {
         }
         for (int i = 0 ; i < temps.length; i++) {
             System.out.println("Kota ke-" + (i + 1) + " : ");
-            for (int j = 0; j < temps[0].length; j++) {
-                    System.out.println(temps[i][j] + " ");
+            for (double temp : temps[i]) {
+                    System.out.println(temp + " ");
             }
+            double sum = 0;
+            for (double temp : temps[i]) {
+                    sum += temp;
+            }
+            double average = sum / temps[i].length;
+            System.out.println("Rata-rata: " + average);
             System.out.println();
-
+        }
     }
- }
 }
