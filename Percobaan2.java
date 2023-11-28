3ff3f053bbdbe2c2db58c02b058979f8c7df48b9
VarizkyNaldiba <138732792+VarizkyNaldiba@users.noreply.github.com>
2023-11-28 20:22:06 +0700

    percobaan 2 rekursif

--- Percobaan2.java
+++ Percobaan2.java
@@ -1,21 +1,27 @@
 import java.util.Scanner;
 
 public class Percobaan2 {
-    static int hitungPangkat (int x, int y) {
+    static int hitungPangkat(int x, int y) {
         if (y == 0) {
-            return (1);
+            System.out.print("1 = ");
+            return 1;
+        } else if (y == 1) {
+            System.out.print(x + " = ");
+            return x;
         } else {
-            return (x * hitungPangkat(x, y - 1));
-
+            System.out.print(x + "x");
+            return x * hitungPangkat(x, y - 1);
         }
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Bilangan yang dihitung : ");
-        int bilangan = sc.nextInt();
-        System.out.println("Pangkat : ");
-        int pangkat = sc.nextInt();
+            System.out.println("Bilangan yang dihitung : ");
+                int bilangan = sc.nextInt();
+            System.out.println("Pangkat : ");
+                int pangkat = sc.nextInt();
 
-        System.out.println(hitungPangkat(bilangan, pangkat));
+            int hasil = hitungPangkat(bilangan, pangkat);
+                System.out.print(hasil);
     }
 }
